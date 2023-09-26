d3798ac33b5ee709ccca75e32420abcd7660b9e1
“RendiPancaWijanarko” <rendi.wijanarko03@gmail.com>
2023-09-26 13:17:50 +0700

    Update kirim data ke wa

---
+++ KirimFeedbackToWA.js
@@ -0,0 +1,29 @@
+function kirimPesan() {
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("email").value;
+    const telepon = document.getElementById("telepon").value;
+    const subjek = document.getElementById("subjek").value;
+    const pesan = document.getElementById("pesan").value;
+
+    if (!nama || !email || !telepon || !subjek || !pesan) {
+        // Menampilkan alert jika ada form input yang belum terisi
+        alert("Harap isi formulir dengan lengkap!");
+    } else {
+        const whatsappText = `Halo,%20apakah%20bisa%20mengatur%20jadwal%20untuk%20kerja%20sama%3F%20Saya%20tertarik%20dengan%20FlyHigh%20Corp.%20%21%0A%0A**Data:**%0ANama%3A%20${nama}%0AEmail%3A%20${email}%0ANo.Telp%3A%20${telepon}%0ASubjek%3A%20${subjek}%0APesan%3A%20${pesan}`;
+        const whatsappLink = `https://wa.me/+6285158911396?text=${whatsappText}`;
+
+        // Mengirim pesan WhatsApp
+        window.open(whatsappLink, "_blank");
+
+        // Menampilkan notifikasi
+        const notifikasi = document.getElementById("notifikasi");
+        notifikasi.innerHTML = `
+            <div class="alert alert-success">
+                Pesan telah terkirim! Terimakasih sudah berpartisipasi.
+            </div>
+        `;
+
+        // Mengosongkan formulir
+        document.getElementById("pesanForm").reset();
+    }
+}
